7599ad45ea31a30a30fbcc6cff26c3c84d4e0a42
Denis O <denis@universal-development.com>
2016-09-03 23:44:17 +0300

    PolyStorageWithMetadata

---
+++ polydata-storage/src/main/java/com/unidev/polydata/storage/PolyStorageWithMetadata.java
@@ -0,0 +1,17 @@
+package com.unidev.polydata.storage;
+
+import com.unidev.polydata.domain.Poly;
+
+/**
+ * Poly storage with metadata
+ */
+public interface PolyStorageWithMetadata extends PolyStorage  {
+
+    /**
+     * Fetch storage metadata
+     * @return Storage metadata
+     */
+    <T extends Poly> T fetchMetadata();
+
+
+}
