f1f1dd94ec9fb5f780889fe74a2270894f3ac255
Denis <denis@universal-development.com>
2016-11-01 11:16:51 +0200

    Added poly indexer api methods

--- polydata-storage/src/main/java/com/unidev/polydata/storage/PolyIndexer.java
+++ polydata-storage/src/main/java/com/unidev/polydata/storage/PolyIndexer.java
@@ -1,7 +1,43 @@
+/**
+ * Copyright (c) 2015,2016 Denis O <denis@universal-development.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.unidev.polydata.storage;
+
+import com.unidev.polydata.domain.Poly;
 
 /**
  * Indexer of polydata records
  */
 public interface PolyIndexer {
+
+    /**
+     * Handle adding new poly to storage
+     * @param poly
+     */
+    void added(Poly poly);
+
+    /**
+     * Method invoked when poly is updated in storage
+     * @param poly
+     */
+    void updated(Poly poly);
+
+    /**
+     * Handle case when poly is removed from storage
+     * @param poly
+     */
+    void removed(Poly poly);
+
 }
