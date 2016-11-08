449f67a872a56251ec016e25507d9ebf015ab3fa
Denis O <denis@universal-development.com>
2016-11-08 22:08:33 +0200

    Using PolyServices instead of indexes

--- polydata-storage/src/main/java/com/unidev/polydata/storage/PolyStorage.java
+++ polydata-storage/src/main/java/com/unidev/polydata/storage/PolyStorage.java
@@ -18,7 +18,6 @@ package com.unidev.polydata.storage;
 import com.unidev.polydata.domain.Poly;
 
 import java.util.Collection;
-import java.util.Map;
 
 /**
  * Generic poly storage
@@ -30,12 +29,6 @@ public interface PolyStorage {
      * @return
      */
     <P extends Poly> P metadata();
-
-    /**
-     * Fetch storage indexes, mapped by name
-     * @return
-     */
-    Map<String, ? extends PolyStorage> index();
 
     /**
      * Fetch poly record by id
