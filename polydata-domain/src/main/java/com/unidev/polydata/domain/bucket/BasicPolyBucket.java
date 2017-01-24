5e1b2a6274ebe8522dfdce810f55a5760182fa86
Denis <denis@universal-development.com>
2017-01-24 21:35:18 +0200

    Basic poly bucket implementation

---
+++ polydata-domain/src/main/java/com/unidev/polydata/domain/bucket/BasicPolyBucket.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2017 Denis O <denis@universal-development.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.unidev.polydata.domain.bucket;
+
+import com.unidev.polydata.domain.BasicPoly;
+import com.unidev.polydata.domain.BasicPolyList;
+
+/**
+ * Basic poly bucket
+ */
+public class BasicPolyBucket implements PolyBucket {
+
+    private BasicPoly metadata;
+    private BasicPolyList polys;
+
+    public static BasicPolyBucket newBucket() {
+        return new BasicPolyBucket();
+    }
+
+    @Override
+    public BasicPoly metadata() {
+        return metadata;
+    }
+
+    @Override
+    public BasicPolyList polys() {
+        return polys;
+    }
+
+    public BasicPoly getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(BasicPoly metadata) {
+        this.metadata = metadata;
+    }
+
+    public BasicPolyList getPolys() {
+        return polys;
+    }
+
+    public void setPolys(BasicPolyList polys) {
+        this.polys = polys;
+    }
+}
